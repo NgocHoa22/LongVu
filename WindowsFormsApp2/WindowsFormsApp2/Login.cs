44be0828cd1a2931bb49496a02073e3d7ac0b563
NgocHoa22 <hoa228205hhh@gmail.com>
2024-08-12 15:13:39 +0700

    Add files via upload

---
+++ WindowsFormsApp2/WindowsFormsApp2/Login.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Login : Form
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            DialogResult result = MessageBox.Show(
+              "You want to ESC",
+              "confirm",
+              MessageBoxButtons.YesNo,
+              MessageBoxIcon.Question
+              );
+            if (result == DialogResult.Yes)
+            {
+                Close();
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (txtpass.Text == txtcf.Text)
+            {
+                Form1 from1 = new Form1();
+                from1.Show();
+                this.Hide();
+            }
+        }
+    }
+}
